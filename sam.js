009d1e5b5248d633f95b91b178ad2b3429b5414f
tokihoise <70904575+tokihoise@users.noreply.github.com>
2020-09-07 20:47:03 +0600

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,438 @@
+
+		 		
+ 
+	
+	
+	//document.write("<p style=color:red;text-align:center;font-size:7vw;>Watashi wa uzumaki naruto</p>");
+	//console.log("My name is saliho ahmed ");
+	//Ara ara//
+	//console.log(toString,parseFloat,parseInt)
+	
+	
+	//var number =43.53759;
+	
+	//console.log(number.toPrecision(2))
+	//document.write("Saliho Ahmed"+" Mehar");
+	 
+	//var firstName= "Saliho Ahmed"; 
+	//var lastName= "Mehar"; 
+	//var full= firstName + lastName;
+	//document.write("My name is"+full);
+	
+	//var num1 =32;
+	//var num2 =21;
+	
+	//document.write("Num1 ="+num1+"Num2 ="+num2);
+	
+	//var text = "Bangladesh worst country"
+	//var len = text.length;
+	//document.write(len);
+	
+	//var text =prompt("Enter your name");
+	
+	//document.write("number of character"+text.length);
+	
+	
+	//var text="sasageyo sasageyo";
+	//document.write(text.charAt(2));
+	
+	//var text="sasageyo sasageyo";
+	//text=text.toUpperCase();
+	//document.write(text);
+	
+	//var text2="sasageyo sasageyo";
+	//var text1="sasageyo sasageyo";
+	//var text3= text1.concat(text2);
+	//document.write(text3);
+	
+	//var text2="sasageyo sasageyo";
+	
+	//var text3= text2.slice(3,9);
+	//document.write(text3);
+	
+	//+,-,*,/,**(to the power),%(bhagshesh)//
+	//var a=8766, a++,a--,--a,++a//
+	
+		//var x=8;
+	//x -= 6;
+	//document.write(x);
+	
+ //var num1 = prompt("Enter the first number");	
+	
+	//var num2 = prompt("Enter the second number");	
+	
+	
+	//num1 = parseInt(num1, 10);
+	//num2 = parseInt(num2, 10);
+
+
+//	var sum,sub,multiple,divide; 
+	//sum = num1 + num2;
+	//document.write(num1+"+"+num2+ "="+sum+"<br/>");
+	
+//	sub = num1 - num2;
+	//document.write("Substraction is ="+sub+"<br/>");
+	
+//	multiple = num1 * num2;
+	//document.write("Multiplication  is ="+multiple+"<br/>");
+	
+//	divide = num1 / num2;
+	//document.write("Quotient is ="+divide+"<br/>");
+	
+	
+	
+	//var farn = prompt("Enter the farenhite temperature you want to convert in Celsius ")
+	
+	//var cels = (farn-32)*5/9
+	
+	
+	
+	//document.write("Celsius temperature is =" + cels);
+//var num1 = prompt("Enter Your Mark");
+	
+//	var num2,num3,num4,num5;
+	
+//	num2=80;
+//	num3=60;
+//	num4=40;
+//	num5=0;
+	
+	
+//	if(num1>=num2&&num1<=100)
+//	document.write("You got A+");
+
+//else if(num1>=num3&&num1<=100) 
+	
+	//document.write("You got B+");
+	
+//else if (num1>=num4&&num1<=100)
+	
+	//document.write("You got C");
+	
+//else if(num1<=33 &&num1<=100 &&num1>=0)
+	//document.write("You have failed");
+	
+	//var letter= prompt("Enter a letter");
+	
+	//if(letter=="a" || letter=="i"|| letter=="e"|| letter=="o" || letter=="u"|| letter=="A"|| letter=="E"|| letter=="I"|| letter=="O"|| letter=="U")
+//		document.write("Vowel");
+	
+//else
+//document.write("Consonant");	
+	
+//	var n= prompt ("Digit de");
+	
+	//switch(n){
+		//case"0":
+		//document.write("Zero");
+		//break;
+		//case"1":
+		//document.write("One");
+		//break;
+		//case"2":
+		//document.write("Two");
+		//break;
+		//case"3":
+		//document.write("Three");
+		//break;
+		//case"4":
+		//document.write("Seven");
+		//break;
+		//case"6":
+		//document.write("Six");
+		//default:
+		//document.write("Baal diso");
+		
+		
+		//for(var x=1; x<=10; x=x+2){
+			//document.write("<h1>Sasageyo</h1>");
+		//}
+	
+	
+	//for (var x = 10; x >=1; x = x - 1){
+		//	document.write(" " + x);
+//	}
+	
+	
+	//var sum= 0;
+	
+	//for (var x = 1; x<=100; x=x+1){
+	//sum = sum + x;}
+	//document.write(sum);
+	
+	
+	//var n = parseInt(prompt("Enter the first number "));
+
+		//var m = parseInt(prompt("Enter the Last number "));
+	
+//	var sum = 0;
+	//for (var x = n; x<= m ; x=x+1 ){
+	//sum= sum + x;}
+	
+//	document.write("Lo tor jogfol= " + sum);
+	
+	
+	
+	//var x=1;
+	//while (x<=99){
+		
+		//document.write(" " + x);
+		//x=x+1;
+	//}
+
+
+
+	
+	//var x= 1;
+	
+	//var sum = 0;
+	//while (x<=100){
+		
+		
+	
+		//sum=sum+x;
+		
+		
+	//	x=x+1;
+	//}
+//document.write(" "+ sum);
+
+
+
+
+	
+	//var x= 15;
+//var sum = 0;
+	//while (x<=100){
+		
+	// if(x%3==0 && x%5==0)	{
+	//	 sum=sum+x;
+	//document.write(" "+x);
+	//}		
+//		x=x+1;
+	//}
+//document.write(" "+ sum);
+
+
+
+
+
+
+// var sum =0;
+ 
+// for ( x=1; x<=100; x=x+1){
+	 
+	// if(x%3==0 && x%5==0)
+	 
+	// sum = sum +x;
+ //}
+//document.write(" "+ sum);
+
+
+
+//var x=1;
+//var sum =0;
+
+ //do{
+	
+	//	sum = sum + x;
+		//x=x+1;
+ //}
+ //while(x<=900);
+ 
+// document.write(" "+ sum);
+
+
+
+//for(x=1;x<=100;x=x+1){
+	
+	//if (x%2==0){
+	//	continue othoba break;
+	//}
+	
+//	document.write(" " + x);
+//}
+
+//	var sum =0;
+//	for(x=1;x<=100;x=x+1){
+		
+	//	sum= x*x;
+	//	document.write( "Result is = "+ sum + "<br>");
+	//}
+	
+//	function square(num1, num2){
+		
+	//	var result = num1*num2;
+		//document.write("Lo to Gunfol ="+ result+ "<br>");
+	//}
+	//square(5, 6);
+	
+	
+	
+	//var names= ["Saliho " , "Ahmed " , "Mehar" , " is "];
+	//names.push("ndjdhno");
+	//names.pop();names.shift();names.unshift();
+	
+	//adding element-.-
+	//names.splice(2,1,"saliho","saliho");
+	
+	
+	//remove-.-
+	//names.splice(2,1,);
+	
+	//document.write(names[1]);
+//	document.write(names);
+	
+
+
+	//var	country1=["sfsff" ,"dfsfs"];
+	/* //var country2=["wfyry","yedee"]; */
+	
+//	var country=country1.concat(country2);
+	
+//	document.write(country);
+
+
+
+	
+	//var num=[1,22,3,4,2,7];
+	//var sum = 0;
+	//for(var x=1; x<=5; x=x+1){
+//		sum = sum + num[x];
+//	}
+
+//document.write(" sum="+ sum);
+
+
+
+
+//var num1 = parseInt(prompt("A"));
+//var num2 = parseInt(prompt("A"));
+
+//var sum = Math.max(num1,num2);
+
+//document.write(" "+sum );
+
+
+//numwon =0;
+
+
+//numlost =0;
+
+ //for(x=1;x<=5;x++){var guess=parseInt(prompt("Guess a number betweem 1 to 5"));
+
+//var random1 = Math.floor(Math.random()*5)+1;
+
+//if(guess==random1){
+	//console.log("You have won");
+	//numwon++;
+
+//}
+
+//else{
+//	console.log("You have lost . Random number was =" + random1);
+//	numlost++;
+//}
+
+//}
+//	document.write("Number of won =" + numwon +"<br/>");
+
+//document.write("Number of lost =" + numlost + "<br/>");
+
+
+
+//document.getElementById("headd").innerHTML = "Hlww";
+
+
+
+//document.getElementsByTagName("h1")[1].innerHTML = "Hlww";
+	
+	//document.querySelector("#headd1").innerHTML = "Hlww";
+//	document.querySelector("h2").innerHTML = "Hlww";
+
+/* document.querySelector("li a").innerHTML = "Hlww"; 
+
+*/
+
+	 /* function ailazadu(){
+		alert("Ara ara ehane aisos k?");
+		
+	}
+
+ 
+	 function mama(){
+		alert("Ara ara ehane aisos k?");
+		
+	}
+ */
+ /*  function ailazadu(){
+		document.querySelector("#haha").innerHTML="Why do you click"; */
+		
+/* 	}  */
+
+
+/* var myVar = document.querySelector("#haha1");
+	function ailazadu1(){
+		
+		myVar.src="https://shareitnow.net/wp-content/uploads/2019/12/Hatake-Kakashi-quotes.jpg"
+	} 
+	function ailazadu2(){
+		
+		
+		myVar.src="https://i.pinimg.com/originals/a6/65/6f/a6656f5d7f2d0074993dca19278d6774.jpg"
+	}  */
+	/*Wrong------ function nex(){for(x=0; x<=3;x++)
+		
+		{"https://shareitnow.net/wp-content/uploads/2019/12/Hatake-Kakashi-quotes.jpg""https://i.pinimg.com/originals/a6/65/6f/a6656f5d7f2d0074993dca19278d6774.jpg"
+	} 
+	}
+	
+	function pre(){
+		
+		for(x=3; x>=0;x--){
+			
+			"https://i.pinimg.com/originals/a6/65/6f/a6656f5d7f2d0074993dca19278d6774.jpg""https://shareitnow.net/wp-content/uploads/2019/12/Hatake-Kakashi-quotes.jpg"
+		}
+
+	}  */
+/* 	
+	var photos =["https://shareitnow.net/wp-content/uploads/2019/12/Hatake-Kakashi-quotes.jpg","https://i.pinimg.com/originals/a6/65/6f/a6656f5d7f2d0074993dca19278d6774.jpg","https://qph.fs.quoracdn.net/main-qimg-5c00a6f0a71af8bfe6b0f6d2fdface41"];
+	
+	
+	var imgtag = document.querySelector("#naruto");
+	
+	count=0;
+	
+	function nex(){
+		
+		count++;
+		if(count>=photos.length){
+			
+			count=0;
+			imgtag.src= photos[count];
+		
+		}
+		
+		else{imgtag.src= photos[count];}
+		
+	}
+	function pre(){
+		
+		count--;
+		if(count< 0){
+			
+			count=photos.length -1;
+			imgtag.src= photos[count];
+		
+		}
+		
+		else{imgtag.src= photos[count];}
+		
+	}
+	 */
+	
+	
+	
+	
+	
+	
